bc8c9e639191a358703b5797a2bfe673951c750d
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2025-11-19 23:45:11 -0600

    project outline

---
+++ src/config/config.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+basedir = Path(__file__).resolve().parent.parent
+envfile = basedir / ".env"
+load_dotenv(str(envfile))
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    GITHUB_API_KEY = os.environ.get("GITHUB_API_KEY")
+    JIRA_API_KEY = os.environ.get("JIRA_API_KEY")
+    ADMINS = ["arthurvargasdev@gmail.com"]
+    ITEMS_PER_PAGE = 10
+    LANGUAGES = ["en", "es"]
